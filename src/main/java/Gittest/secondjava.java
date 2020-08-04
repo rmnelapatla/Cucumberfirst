20f91d2a541439be91ad0248692888b0489b3575
Rmana <siwakoti.prakash9@gmail.com>
2020-08-04 07:08:19 +0530

    changed error

--- src/main/java/Gittest/secondjava.java
+++ src/main/java/Gittest/secondjava.java
@@ -5,7 +5,9 @@ public class secondjava {
     public static void main(String ar[]){
         int age=99;
         System.out.println(age);
+
+        System.out.println("this is another file");
     }
 
-  System.out.println("this is another file");
+
 }
